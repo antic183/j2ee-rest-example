970d2198e04c16cf0863ddd222b4bd39f6dc2c4b
Antic <antic83@gmail.com>
2017-04-15 00:29:03 +0200

    code formatting

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,29 +1,30 @@
 (function($) {
-	$(document).ready(function() {
-		$(document).on('submit', '#myForm', function(e) {
-			e.preventDefault();
-			var formData = $(this).serializeJSON({parseBooleans: true});
-			    data = formData.dataType !== 'Json'
-			    	 		? {firstName: formData.firstName, lastName: formData.lastName}
-							: JSON.stringify({firstName: formData.firstName, lastName: formData.lastName});
+  $(document).ready(function() {
+    $(document).on('submit', '#myForm', function(e) {
+      e.preventDefault();
 
-			$.ajax({
-				url : $(this).attr('action') + formData.pathParam,
-				dataType : formData.dataType,
-				cache : false,
-				contentType : formData.contentType,
-				processData : formData.processData,
-				data : data,
-				type : formData.requestMethode,
-				success : function(res) {
-					console.info(res);
-				},
-				fail : function(err) {
-					console.warn(err);
-				}
-			});
-		});
+      var formData = $(this).serializeJSON({parseBooleans: true}),
+          data = formData.dataType !== 'Json'
+                 ? {firstName: formData.firstName, lastName: formData.lastName}
+                 : JSON.stringify({firstName: formData.firstName, lastName: formData.lastName});
 
-		$(document).foundation();
-	});
+      $.ajax({
+        url : $(this).attr('action') + formData.pathParam,
+        dataType : formData.dataType,
+        cache : false,
+        contentType : formData.contentType,
+        processData : formData.processData,
+        data : data,
+        type : formData.requestMethode,
+        success : function(res) {
+          console.info(res);
+        },
+        fail : function(err) {
+          console.warn(err);
+        }
+      });
+    });
+
+    $(document).foundation();
+  });
 })(jQuery);
